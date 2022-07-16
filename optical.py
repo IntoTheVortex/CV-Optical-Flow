0a0869b76d8c3e423206e065a4fae6b444a55856
IntoTheVortex <amberadelaideshore@gmail.com>
2022-07-16 12:39:24 -0700

    vectors better, fixed washout

--- optical.py
+++ optical.py
@@ -8,8 +8,8 @@ import cv2 as cv
 
 
 
-def get_image_padded(image):
-    p = 1 #how many zeros to pad the matrix with
+def get_image_padded(image, p):
+    #p = 1 #how many zeros to pad the matrix with
         
     image = image/255 #normalize
     image_padded = np.pad(image, ((p,p),(p,p)), 'constant')
@@ -25,18 +25,23 @@ def get_pixel_intensity_difference(image1, image2):
 def convolve(data, _filter, p):
     n = len(data)
     m = len(data[0])
+    data = get_image_padded(data, p)
     f = len(_filter)
     new_shape_i = (n + 2 * p - f + 1)
     new_shape_j = (m + 2 * p - f + 1)
     convolved = np.zeros(shape=(new_shape_i,new_shape_j))
+    print("new conv shape:", convolved.shape)
 
     #stride = 1
     for i in range(0,n):
         for j in range(0,m):
-            if i+f > n or j+f > m:
-                continue
-            a = np.asarray(data[i:i+f, j:j+f])
-            convolved[i][j] = sum(sum(a * _filter))
+            if i+f < n and j+f < m:
+                a = np.asarray(data[i:i+f, j:j+f])
+                #print("a:", a.shape)
+                #print("f:", f)
+                #print("i:", i)
+                #print("i+f:", i+f)
+                convolved[i][j] = sum(sum(a * _filter))
 
     return convolved
 
@@ -53,21 +58,19 @@ def create_dfilters():
 def ols_flow(Ix, Iy, Itp):
     n = len(Ix)
     m = len(Ix[0])
-    f = 2 
+    f = 3 
 
     print('ix shape:', Ix.shape)
     V = np.zeros([n,m,2])
     print('v shape:', V.shape)
     print('it shape:', Itp.shape)
-    for i in range(1,n-2):
-        for j in range(1,m-2):
-            if i+f > (n-2) or j+f > (m-2):
-                continue
-            else:
+    for i in range(0,n-2):
+        for j in range(0,m-2):
+            if i+f < n and j+f < m:
                 #get A, then compute A transpose A:
                 #print('i:',i, 'j:',j, 'ix:',Ix[i][j], 'iy',Iy[i][j])
-                ax = np.asarray(Ix[i-1:i+f, j-1:j+f].flatten()).T 
-                ay = np.asarray(Iy[i-1:i+f, j-1:j+f].flatten()).T
+                ax = np.asarray(Ix[i:i+f, j:j+f].flatten()).T 
+                ay = np.asarray(Iy[i:i+f, j:j+f].flatten()).T
                 A = np.stack((ax,ay), axis=1)
                 #print('A:81')
                 #print(A.shape)
@@ -83,7 +86,7 @@ def ols_flow(Ix, Iy, Itp):
                     inv_AtA = np.linalg.pinv(AtA)
 
                 #get Atb, then complete calculation V = AtA * Atb
-                b = np.asarray(Itp[i-1:i+f, j-1:j+f]).flatten().T
+                b = np.asarray(Itp[i:i+f, j:j+f]).flatten().T
                 Atb = np.matmul(A.T, b)
                 V[i][j] = np.matmul(inv_AtA, Atb)
 
@@ -99,24 +102,28 @@ def plot_vectors(image, V):
                 y1 = int(y + V[x][y][1]) 
                 #print('x', x1, 'y', y1)
                 #cv.arrowedLine(image, (x,y), (x1,y1), color=color)
-                mask = cv.line(image, (x,y), (x1,y1), color=(0,.8,0), thickness=1)
+                #cv2 read in height by width, so x and y are switched for plotting
+                image = cv.line(image, (y,x), (y1,x1), color=(0,.8,0), thickness=1)
 
-    image = cv.add(image, mask)
-    #image = np.asarray(np.clip(image*255, 0, 255), dtype=np.uint8);
+    #image = cv.add(image, mask)
     cv.imwrite("test.png", image)
     
 
 def main():
+    p = 1
     name1 = 'frame1_a.png'
     name2 = 'frame1_b.png'
     image1 = cv.imread(name1, cv.IMREAD_GRAYSCALE)
+    print("orig",image1.shape)
     image2 = cv.imread(name2, cv.IMREAD_GRAYSCALE)
-    image_padded = get_image_padded(image1)
+    #image_padded = get_image_padded(image1, p) #and scaled
 
-    Itp = get_pixel_intensity_difference(image1, image2)
+    Itp = get_pixel_intensity_difference(image1, image2) #scaled here
     gx, gy = create_dfilters()
-    Ix = convolve(image_padded, gx, 1) 
-    Iy = convolve(image_padded, gy, 1) 
+    Ix = convolve(image1, gx, p) 
+    print("image pad:", image1.shape)
+    print("ix:", Ix.shape)
+    Iy = convolve(image1, gy, p) 
     V = ols_flow(Ix, Iy, Itp)
     #print(V)
     plot_vectors(image1, V)
